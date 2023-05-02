a654fc49eaa3490cf16a7aec53f68bafcb5aba00
Vuong <fanvuong99@gmail.com>
2023-05-02 09:15:06 +0700

    fourth commit

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,4 +1,6 @@
 import db from '../models/index';
+// import user from '../models/user';
+import CRUDService from '../services/CRUDService'
 
 let getHomePage = async (req, res) => {
     try {
@@ -16,16 +18,17 @@ let getAboutPage = (req, res) => {
     return res.render('test/about.ejs');
 }
 
-let getCRUD = (req,res) =>{
-    return res.send('11111111111')
+let getCRUD = async (req,res) =>{
+    return res.render('crud.ejs');
 }
 
-// let postCRUD = async (req, res) => {
-//     let message = await CRUDService.createNewUser(req.body);
-//     console.log(message);
-//     return res.send('post crud from server');
+let postCRUD = async (req, res) => {
+    let message = await CRUDService.createNewUser(req.body);
+    console.log(message);
+    
+    return res.send('post crud from server');
 
-// }
+}
 
 // let displayGetCRUD = async (req, res) => {
 //     let data = await CRUDService.getAllUser();
@@ -80,7 +83,7 @@ module.exports = {
     getHomePage: getHomePage,
     getAboutPage: getAboutPage,
     getCRUD: getCRUD,
-    // postCRUD: postCRUD,
+    postCRUD: postCRUD,
     // displayGetCRUD: displayGetCRUD,
     // getEditCRUD: getEditCRUD,
     // putCRUD: putCRUD,
